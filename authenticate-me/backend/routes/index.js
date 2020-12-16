9c062661c306b95a8cf9c39a19b9d9d79b69f3c3
Samantha Butler <samibutler1992@gmail.com>
2020-12-16 15:34:07 -0700

    initialize api routes

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api')
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
 
+router.use('/api', apiRouter);
 
 router.get('/hello/world', csrfProtection, function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
