b8904e4e3812455302da7fb11e724b3bf19a71ab
Samantha Butler <samibutler1992@gmail.com>
2020-12-17 00:49:52 -0700

    fixed csurf error

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -11,10 +11,33 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 
 const app = express();
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+  // helmet helps set a variety of headers to better secure your app
+  app.use(helmet({
+    contentSecurityPolicy: false
+  }));
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true,
+      },
+    })
+  );
+
+  app.use(routes);
 
 /************  ERROR HANDLING  *************/
 
@@ -38,37 +61,5 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
-// Error formatter
-app.use((err, _req, res, _next) => {
-  res.status(err.status || 500);
-  console.error(err);
-  res.json({
-    title: err.title || 'Server Error',
-    message: err.message,
-    errors: err.errors,
-    stack: isProduction ? null : err.stack,
-  });
-});
-
-// Security Middleware
-if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
-  // helmet helps set a variety of headers to better secure your app
-  app.use(helmet({
-    contentSecurityPolicy: false
-  }));
-
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
-    })
-  );
 
   module.exports = app;
--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api')
-const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
+// const csrf = require('csurf');
+// const csrfProtection = csrf({ cookie: true });
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', csrfProtection, function(req, res) {
+router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
